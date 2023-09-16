735c3720ab66d5d2b86c74b7b534c44c1eff3b1a
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-09-16 17:08:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";  //treat all JS code as newer Version
+
+//alert(3+3) //we are using nodejs, not browser
+
+let name = "avnish"
+let age = 18
+let isLoggedIn = false
+let state ;
+//number =>2 to power 53
+//bigint
+//String => ""
+//boolean = > true/false
+//null =>standalone value
+//undefined =>
+//Symbol =>unique
+
+//object
+console.log(typeof "hitesh");
