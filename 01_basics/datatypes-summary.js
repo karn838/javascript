989e3ca55209247ec3e3cd91dc8e7b8bc222371c
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-09-17 16:40:40 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+//Primitive
+//7 types : String,Number, Boolean, Null, Undefined, Symbol,BigInt
+
+//Javascript is dynamically typed language. this means that variable types are determined at runtime
+
+
+const isLoggedIn = false;
+const outsideTemp =null;
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId)
+const bigNumber = 238943804389403n
+
+//Reference(No primitive)
+//Array, Objects, Functions
+const heros = ["saktiman","doga","naagraj"]
+ let myobj = {
+    name:"avnish",
+    age : 22
+}
+
+const myFunction = function(){
+   console.log("hello world");
+}
+
+/*
+   type of val                          result
+    undefined                           undefined
+    Null                                 object
+    Boolean                              boolean
+    Number                                number
+    String                               string
+    function                             object function
+    array                                  function
+
+*/
