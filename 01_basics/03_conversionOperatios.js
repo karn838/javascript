e7cfe7924f9bdfe408947a73b9c7c06323dc9b6c
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-09-17 15:24:26 +0000

    conversion_operations

--- 01_basics/03_conversionOperatios.js
+++ 01_basics/03_conversionOperatios.js
@@ -19,3 +19,11 @@ let someNumber = 33;
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);
+console.log(+true);
