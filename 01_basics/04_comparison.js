a248f2b7e8b51909cc40384e1fe1b87fe729ca3e
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-09-17 15:40:45 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined==0);   
+//equality == and >= or <= these comparison works differently from equality
+
+//=== check datatype and value both
+//we need to avoid these types of conversion because it create confusion
