3dd53f46bc631fb950e681fe77478ea038052967
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-09-18 05:10:43 +0000

    math and nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,33 @@
+const score = 400;
+//console.log(score);
+
+const balance  = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+// console.log(typeof balance);
+
+const otherNumber = 123.89;
+//console.log(otherNumber.toPrecision(3));
+
+const hunndreds = 1000000
+//console.log(hunndreds.toLocaleString('en-In'));
+
+//++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(3.9));
+// console.log(Math.min(2,5,3,5,6));
+// console.log(Math.max(4,5,7,4,5,2,6));
+
+console.log(Math.random()); //give the value range from 0 to 1
+console.log((Math.random()*10)+1); //now value will be greater than 1
+console.log((Math.random()*10)+1);
+
+const min=10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)  //give value between range
