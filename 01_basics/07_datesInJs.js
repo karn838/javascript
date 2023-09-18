43abae421a08e6f0f7c815fa05474f7400612142
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-09-18 06:16:11 +0000

    date and time

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,28 @@
+// let myDate  = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString());
+// console.log( typeof myDate);
+
+// let createdDate = new Date(2023 , 0 , 23)
+//let createdDate = new Date(2023 , 0 , 23, 5 ,3)
+//let createdDate = new Date("2023-01-13");
+let createdDate = new Date("11-23-2023")
+//console.log(createdDate.toLocaleString());
+
+let mytimeStamp = Date.now();
+//console.log(mytimeStamp);
+//console.log(createdDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate  = new Date()
+console.log(newDate);
+console.log(newDate.getDate())
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
+
+//`{newDate.getDAy()} and the time`
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
