c751e31e200bff71ec4b7efc2c9257d4eb4630cc
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-09-21 06:38:53 +0000

    arrays part-2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -3,7 +3,7 @@ const dc_heros = ["Superman","flash","batman"]
 
 // marvel_heros.push(dc_heros); //place array inside array 
 // console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+ console.log(marvel_heros[3][1]);
 
 // const allHeros = marvel_heros.concat(dc_heros)  //stores in a new array
 // console.log(allHeros)
