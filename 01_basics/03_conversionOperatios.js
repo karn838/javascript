d97ee8692b3b4589032c4f366459c556bf71f388
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-09-21 06:37:29 +0000

    conversion Operator

--- 01_basics/03_conversionOperatios.js
+++ 01_basics/03_conversionOperatios.js
@@ -1,10 +1,10 @@
-// let score = "33";
+ let score = "33";
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
 // console.log(valueInNumber)
 
 //"33" =>33
