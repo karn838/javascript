e5d251014a0fc8dd6be101f85abe73dd0d83d3bd
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-09-25 18:50:06 +0000

    while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+
+// let index=0
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index+=2;
+// }
+
+// let myArray = ['flash','batman','superamn']
+// let arr=0;
+// while (arr<myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr+=1;
+// }
+
+
+// do-while   loop
+let score =11
+do{
+  console.log(`score is ${score}`)
+  score++
+}while(score<=10)
