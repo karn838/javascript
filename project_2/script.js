1c1be303b45592a49ac03b26f7a7d23f15933d30
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-10-13 07:57:34 +0000

    Project-2

---
+++ project_2/script.js
@@ -0,0 +1,26 @@
+
+const form = document.querySelector('form')
+
+//this usecase will give you empty 
+//const height = parseInt(document.querySelector('#Height').value)
+form.addEventListener('submit' ,function(e){
+    e.preventDefault()
+
+   const height= parseInt(document.querySelector('#Height').value)
+   const weight =  parseInt(document.querySelector('#Weight').value)
+   const results = document.querySelector('#results')
+
+   if(height==='' || height<0 || isNaN(height)){
+            results.innerHTML=`please give a valid height ${height}`;
+   }
+   else if (weight==='' || weight<0 || isNaN(weight)){
+    results.innerHTML=`please give a valid weight ${weight}`
+   }
+   else{
+          const bmi = (weight/((height*height)/10000)).toFixed(2);
+          //to show result
+          results.innerHTML=`<span>${bmi}</span>`;
+          
+
+   }
+})
