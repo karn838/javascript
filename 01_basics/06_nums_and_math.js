0d8b2d0a2c49f1e5d16e1b4885746f4c8deaa582
Avnish Kumar karn <84834386+karn838@users.noreply.github.com>
2023-11-07 07:37:56 +0000

    01_basics

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -4,7 +4,7 @@ const score = 400;
 const balance  = new Number(100)
 // console.log(balance);
 
- //console.log(balance.toString().length);
+ console.log(balance.toString().length);
  //console.log(balance.toFixed(2));
  //console.log(typeof balance);
 
--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -3,17 +3,18 @@
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString())
 // console.log(myDate.toLocaleDateString());
- console.log( typeof myDate);
+// console.log(myDate)
+//  console.log( typeof myDate);
 
-// let createdDate = new Date(2023 , 0 , 23)
+let createdDate = new Date(2023 , 0 , 23)
 //let createdDate = new Date(2023 , 0 , 23, 5 ,3)
 //let createdDate = new Date("2023-01-13");
-let createdDate = new Date("11-23-2023")
+//let createdDate = new Date("11-23-2023")
 //console.log(createdDate.toLocaleString());
 
-let mytimeStamp = Date.now();
-//console.log(mytimeStamp);
-//console.log(createdDate.getTime());
+// let mytimeStamp = Date.now();
+// console.log(mytimeStamp);
+// console.log(createdDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate  = new Date()
